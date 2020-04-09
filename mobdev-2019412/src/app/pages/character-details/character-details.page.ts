054bfc550cada6e830b4fc427017e7f1aa4f7546
jes22santos <jes22santos@gmail.com>
2020-04-09 11:16:57 +0000

    back botton + style

--- mobdev-2019412/src/app/pages/character-details/character-details.page.ts
+++ mobdev-2019412/src/app/pages/character-details/character-details.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';	
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-character-details',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailsPage implements OnInit {
 
-  constructor() { }
+  characterId = null;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+      this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
--- mobdev-2019412/src/app/pages/characters/characters.page.ts
+++ mobdev-2019412/src/app/pages/characters/characters.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';	
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+  constructor(private navController: NavController, private router: Router) { }
 
   ngOnInit() {
   }
 
+  openDetails() {
+    
+    this.router.navigateByUrl(`/tabs/characters/51`);
+  }
 }
