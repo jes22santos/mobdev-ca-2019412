5110a44e4d0d596a9c8b6e717ab6c0f3460625b0
jes22santos <jes22santos@gmail.com>
2020-04-09 10:38:39 +0000

    Creating tab

--- mobdev-2019412/src/app/pages/tabs/tabs-routing.module.ts
+++ mobdev-2019412/src/app/pages/tabs/tabs-routing.module.ts
@@ -5,8 +5,59 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
+          }
+        ]
+      },
+            {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+          },
+          {
+            path: ':id',
+            loadChildren: () => import('../episode-details/episode-details.module').then( m => m.EpisodeDetailsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+          }
+        ]
+      },
+      {
+        path: 'deathCount',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../death-count/death-count.module').then( m => m.DeathCountPageModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
+    redirectTo: '/tabs/characters',
+    pathMatch: 'full'
   }
 ];
 
